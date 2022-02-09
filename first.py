208c02f546fd40ba30dbe4d54965fd60772fabc3
vlreddy123 <99265985+vlreddy123@users.noreply.github.com>
2022-02-09 18:30:33 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,14 @@
+a=int(input("num1="))
+b=int(input("num2="))
+
+if a>b:
+    print("a is largest number")
+else:
+    print("b is largest number")
+    
+    
+    
+output:    
+num1=10
+num2=5
+a is largest number
